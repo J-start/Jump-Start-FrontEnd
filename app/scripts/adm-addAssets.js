dca700234fe53f6714a932f7eb7794ea28971bc2
MarcosViniSil <marcossilv203@gmail.com>
2025-02-11 19:39:26 -0300

    feat: add change url image assets

---
+++ app/scripts/adm-addAssets.js
@@ -0,0 +1,7 @@
+document.getElementById("new-asset-form").addEventListener("submit" , () =>{
+    
+})
+function addNewAsset(){
+
+    document.getElementById("popup-overlay-new").style.display = "block"
+}
