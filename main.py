d3d3f4fd87833326f77b74f63b6b0a62d4636e1c
MarcosViniSil <marcossilv203@gmail.com>
2025-02-19 12:33:57 -0300

    test: add class to verify whether test passed or failed

--- main.py
+++ main.py
@@ -2,31 +2,53 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+from colorama import Fore, Style
 
 from test.classes.buyAsset import BuyAsset_test
+from test.classes.checkBuyAsset import CheckBuyAsset
 
 driver = webdriver.Chrome() 
-
+listTests = []
+listNameTests = ["Teste de compra","Teste de venda"]
 driver.get("C:/Users/User/Documents/jump-start-front/index.html")
 
-time.sleep(2)
+def BuyAsset():
+    buyAssetObj = BuyAsset_test(driver)
+    time.sleep(2) 
+    buyAssetObj.clickButtonAlertIntialPage()
+    time.sleep(2) 
+    buyAssetObj.loginUserDefault()
+    time.sleep(2) 
+    buyAssetObj.clickButtonCoins()
+    time.sleep(2) 
+    buyAssetObj.clickButtonSeeMoreAboutCoin()
+    time.sleep(2) 
+    buyAssetObj.clickButtonBuyCoin()
+    time.sleep(2) 
+    buyAssetObj.insertQuantityToBuyCoin()
+    time.sleep(2) 
+    buyAssetObj.clickButtonAdvanceBuyCoin()
+    time.sleep(2) 
+    buyAssetObj.clickButtonConfirmationBuyCoin()
 
-buyAssetObj = BuyAsset_test(driver)
-time.sleep(1) 
-buyAssetObj.clickButtonAlertIntialPage()
-time.sleep(1) 
-buyAssetObj.loginUserDefault()
-time.sleep(1) 
-buyAssetObj.clickButtonCoins()
-time.sleep(1) 
-buyAssetObj.clickButtonSeeMoreAboutCoin()
-time.sleep(1) 
-buyAssetObj.clickButtonBuyCoin()
-time.sleep(1) 
-buyAssetObj.insertQuantityToBuyCoin()
-time.sleep(1) 
-buyAssetObj.clickButtonAdvanceBuyCoin()
-time.sleep(1) 
-buyAssetObj.clickButtonConfirmationBuyCoin()
+def checkStatusAfterBuy():
+    checkAssetObj = CheckBuyAsset(driver)
+    listTests.append(checkAssetObj.verifyBuyAssetResponse())
 
-time.sleep(10)
+time.sleep(1) 
+BuyAsset()
+time.sleep(2) 
+
+checkStatusAfterBuy()
+
+time.sleep(3) 
+
+print(Fore.BLUE+f"-------------- RESUMO DOS TESTES -------------- \n"+Style.RESET_ALL)
+
+for index, test in enumerate(listTests):
+    if test:
+        print(Fore.GREEN+f"{listNameTests[index]} Passou"+Style.RESET_ALL)
+    else:
+        print(Fore.RED+f"{listNameTests[index]} Falhou"+Style.RESET_ALL)
+
+print(Fore.BLUE+f"-------------- RESUMO DOS TESTES -------------- \n"+Style.RESET_ALL)
